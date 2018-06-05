86f289b3c9e2592f3905eb1ebdafa61df8e7485c
DataD <dianmante@gmail.com>
2018-06-05 19:52:09 -0400

    KM for NMF mutation top 5

--- R/6-SurvivalAnalysis/6-3v2-Top5curves_NMF_mut_k7.R
+++ R/6-SurvivalAnalysis/6-3v2-Top5curves_NMF_mut_k7.R
@@ -12,7 +12,9 @@ table(surCli[which(as.numeric(surCli$component) %in% bc4),"component"])
 setEPS()
 postscript("output4paper/KM_top5_NMF_mut.eps", width = 12, height = 9)
 plot.Survival4paper(coxFitTop, mfitTop, location = "bottomleft",
-                    colorsP = c("red","black","blue","#1E8449"),
-                    colorsL = c("blue","#1E8449","red","black"),
-                    labelClu = c("subtype 1 (302)","subtype 2 (4)", "subtype 3 (4)", "subtype 4 (7)") )
+                    colorsP = c("red","black","blue","#1E8449", "orange"),
+                    colorsL = c("blue","#1E8449","red","black", "orange"),
+                    labelClu = c("subtype 1 (4)","subtype 2 (302)",
+                                 "subtype 3 (4)", "subtype 4 (10)", "subtype 5 (7)"),
+                    font_size_times = 0.5)
 dev.off()
