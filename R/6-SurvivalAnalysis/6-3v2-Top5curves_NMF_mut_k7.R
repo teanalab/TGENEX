7654dfb97c4896d9ac5116efb47a0ac37a8a52a5
DataD <dianmante@gmail.com>
2018-06-05 19:53:16 -0400

    KM mutation top 5

--- R/6-SurvivalAnalysis/6-3v2-Top5curves_NMF_mut_k7.R
+++ R/6-SurvivalAnalysis/6-3v2-Top5curves_NMF_mut_k7.R
@@ -15,6 +15,5 @@ plot.Survival4paper(coxFitTop, mfitTop, location = "bottomleft",
                     colorsP = c("red","black","blue","#1E8449", "orange"),
                     colorsL = c("blue","#1E8449","red","black", "orange"),
                     labelClu = c("subtype 1 (4)","subtype 2 (302)",
-                                 "subtype 3 (4)", "subtype 4 (10)", "subtype 5 (7)"),
-                    font_size_times = 0.5)
+                                 "subtype 3 (4)", "subtype 4 (10)", "subtype 5 (7)") )
 dev.off()
