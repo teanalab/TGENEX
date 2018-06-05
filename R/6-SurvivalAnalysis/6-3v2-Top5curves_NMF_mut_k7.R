c88ab06a020cda51c8f2a8b0dd5e1a0bd2f952f6
DataD <dianmante@gmail.com>
2018-06-05 19:35:12 -0400

    KM for NMF mutation

---
+++ R/6-SurvivalAnalysis/6-3v2-Top5curves_NMF_mut_k7.R
@@ -0,0 +1,18 @@
+#version 1 (Jun/5/2018)
+
+rm(list = ls())
+load("temp/6-3v1-Top5_NMF_mut_k7.RData")
+loadls("plyr survival missForest survAUC prodlim survminer", F)
+
+#standard plot
+ggsurvplot(mfitTop, conf.int = F,pval = test$sctest[3], risk.table = T )
+
+table(surCli[which(as.numeric(surCli$component) %in% bc4),"component"])
+
+setEPS()
+postscript("output4paper/KM_top5_NMF_mut.eps", width = 12, height = 9)
+plot.Survival4paper(coxFitTop, mfitTop, location = "bottomleft",
+                    colorsP = c("red","black","blue","#1E8449"),
+                    colorsL = c("blue","#1E8449","red","black"),
+                    labelClu = c("subtype 1 (302)","subtype 2 (4)", "subtype 3 (4)", "subtype 4 (7)") )
+dev.off()
