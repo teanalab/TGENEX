906b491f6d76f9797eaf877da6bb8892ac710ec6
DataD <dianmante@gmail.com>
2018-07-27 16:39:45 -0400

    SAnalysis

---
+++ R/6-SurvivalAnalysis/6-8v1-SurvivalAnalysisofNBS.R
@@ -0,0 +1,82 @@
+#version 3 (May/28/2018)
+rm(list = ls())
+load("data/myLib.RData")
+loadls("plyr survival missForest survAUC prodlim survminer", F)
+
+
+LoadMyData <- funtion()
+{
+  #old data
+  load("data/survivalClinical-5-1_30.RData")
+  # load("data/tensorClinical.RData")
+  # row.names(tensorClinical) <- tensorClinical$patient.bcr_patient_barcode
+  # tensorClinical <- tensorClinical[patients,-1]
+
+
+  binaMredu <-read.table(file="nbs_release_v0.2/BCRAdata/10-binaMredu.csv",sep = ";", quote = '"',
+                         header = TRUE, stringsAsFactors = FALSE)
+  clustersNBS <-read.table(file="nbs_release_v0.2/BCRAdata/output_clustersNBS.csv", sep = "\t", quote = "",
+                           header = FALSE, stringsAsFactors = FALSE)
+  standardNMF <-read.table(file="nbs_release_v0.2/BCRAdata/output_standardNMF.csv", sep = ",", quote = "",
+                           header = FALSE, stringsAsFactors = FALSE)
+  genes_redu <-read.table(file="nbs_release_v0.2/BCRAdata/10-genes_redu.csv", sep = "\t", quote = '"',
+                          header = TRUE, stringsAsFactors = FALSE)
+  clinical <-read.table(file="nbs_release_v0.2/BCRAdata/10-clinical.csv",sep = ";", quote = '"',
+                        header = TRUE, stringsAsFactors = FALSE)
+  patients <-read.table(file="nbs_release_v0.2/BCRAdata/10-patients.csv", sep = "\t", quote = '"',
+                        header = TRUE, stringsAsFactors = FALSE)
+
+  survivalClinical <- survivalClinical[as.character(unlist(patients)),]
+
+  #plotting functions
+  source("R/6-SurvivalAnalysis/plotSurvivalUpdate.R")
+  save.image(file="temp/6-8v1-data.RData")
+}
+
+
+#' Survival Analysis
+survAna <- function(){
+  patiF <- data.frame(patients,clustersNBS)
+  row.names(patiF) <- as.character(unlist(patients))
+  patiF <- merge(as.data.frame(patiF), as.data.frame(survivalClinical), by='row.names', all=TRUE)
+  row.names(patiF) <- patiF[,1]
+  patiF <- patiF[,-1]
+  patiF <- patiF[,-1]
+  #NBS components
+  SurFunction <- Surv(time = Overall.Survival..Months.,
+                      event = Overall.Survival.Status)~V1
+  coxFit_NBS <- coxph(SurFunction,
+                        x=T, y=T, model=T, method = "breslow",
+                        data=patiF)
+  summary(coxFit_NBS)
+
+  # Concordance= 0.536  (se = 0.05 )
+  # Rsquare= 0.001   (max possible= 0.606 )
+  # Likelihood ratio test= 0.41  on 1 df,   p=0.5
+  # Wald test            = 0.41  on 1 df,   p=0.5
+  # Score (logrank) test = 0.41  on 1 df,   p=0.5
+
+
+
+  #Kaplan Meier population
+  plot(survfit(coxFit_NBS), main = "NBS")
+
+  #Nonparametric estimation in event history analysis
+  #clinical variables
+  km0 <- prodlim(SurFunction,
+                 method = "breslow",
+                 data=patiF)
+  #Complex Plots
+  mfit <- survfit(SurFunction,
+                  data=patiF)
+  plot.SurvivalComplex (coxFit_NBS, mfit, "NBS")
+
+  #
+  plot.Survival4paper(coxFit_NBS, mfit, location = "bottomleft",
+                      colorsP = c("red","black","blue"),
+                      colorsL = c("blue","black","red"),
+                      labelClu = c("subtype 1","subtype 2", "subtype 3") )
+
+}
+
+save.image("temp/6-8.RData")
